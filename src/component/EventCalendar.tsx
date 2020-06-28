583a9cad8c910258c6457313cf26f9da91130c3b
RidaMohamed <moharedha19@gmail.com>
2020-06-28 16:55:40 +0200

    Adding popup when clicking sur a day

--- src/component/EventCalendar.tsx
+++ src/component/EventCalendar.tsx
@@ -2,13 +2,27 @@ import React from "react";
 
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
 
 class EventCalendar extends React.Component {
   render() {
     return (
-      <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" />
+      <FullCalendar
+        plugins={[dayGridPlugin, interactionPlugin]}
+        initialView="dayGridMonth"
+        dateClick={this.handleDateClick}
+        events={[
+          { title: "event 1", date: "2020-06-01" },
+          { title: "event 2", date: "2020-06-02" },
+        ]}
+      />
     );
   }
+
+  handleDateClick = (arg: any) => {
+    // lorsque une date est cliquee
+    alert(arg.dateStr);
+  };
 }
 
 export default EventCalendar;
